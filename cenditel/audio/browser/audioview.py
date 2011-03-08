2a3a378afae097dd857f69852bc7275bfa84be59
Victor Terán <vteran93@yahoo.es>
2011-03-08 19:05:31 +0000

    Modificada vista .pt que tenia el llamado a un metodo innecesario. Se modifica llamado del metodo transcode__init__ con nuevos parametros que permitan ejecutar de mejor manera los procesos.

--- cenditel/audio/browser/audioview.py
+++ cenditel/audio/browser/audioview.py
@@ -4,7 +4,9 @@ from Products.CMFCore.utils import getToolByName
 ###################
 #python imports
 import urlparse
-import os
+
+from os import path
+from urllib import quote
 #############
 from Acquisition import aq_inner
 from zope.component import getMultiAdapter
@@ -45,7 +47,6 @@ class audioView(BrowserView):
         self.request = request
 	self.MyTitle = ""
 	self.MyTitleWhitOutSpace = ""
-	self.filename = ""
 	self.newfilename = ""
 	self.filenamesaved= ""
 	self.folderfile = ""
@@ -74,39 +75,58 @@ class audioView(BrowserView):
 	registry = getUtility(IRegistry)
 	settings = registry.forInterface(ITranscodeSetings)
 	self.SERVER = self.RemoveSlash(settings.adress_of_streaming_server)
-	PARAMETRES_TRANSCODE = settings.ffmpeg_parameters_audio_line
+	VIDEO_PARAMETRES_TRANSCODE = settings.ffmpeg_parameters_video_line
+	AUDIO_PARAMETRES_TRANSCODE = settings.ffmpeg_parameters_audio_line
+	audio_content_types=settings.audio_valid_content_types
+	video_content_types=settings.video_valid_content_types
 	self.STORAGE = self.RemoveSlash(settings.mount_point_fss)
 	self.MyTitle = self.context.Title()
         idaudio=self.context.getId()
-	"""
-	virtualobject=self.context.getAudio()
-	self.filenamesaved=virtualobject.filename
-	"""
 	self.MyTitleWhitOutSpace = MFNI.TitleDeleteSpace(self.MyTitle) 
-	self.filename = MFNI.DeleteSpaceinNameOfFolderFile(self.MyTitleWhitOutSpace)  
 	url = self.context.absolute_url()
 	self.PathOfFile = MFNI.ReturnPathOfFile(url)
-	self.filenamesaved = MFNI.ReturnFileNameOfFileSaved(self.STORAGE, self.PathOfFile)
-	if self.filenamesaved.haskey('ErrorBol')
-	if self.filenamesaved['ErrorBol']
-	print "IT IS THE NAME OF THE FILE SAVED " + self.filenamesaved
-        #import pdb; pdb.set_trace()
+	virtualobject=self.context.getAudio()
+	self.filenamesaved=virtualobject.filename
+	self.extension=MTDI.CheckExtension(self.filenamesaved)
 	self.MyTitleWhitOutSpace = MFNI.DeleteSpaceinNameOfFolderFile(self.MyTitleWhitOutSpace)
-	newtrans_init_(self.STORAGE, self.PathOfFile, self.filenamesaved, PARAMETRES_TRANSCODE, idaudio)
-	self.folderfileOGG=MTDI.newname(self.PathOfFile+'/' + self.filenamesaved)
-	self.AbsoluteServerPath = self.SERVER + MTDI.nginxpath(self.folderfileOGG)
-	self.newfiletranscoded=MTDI.nginxpath(self.STORAGE+self.folderfileOGG)
-	self.StatusOfFile = ServiceList.available(idaudio,self.newfiletranscoded)
-	print "El STATUS OF FILE IN THE VIEW "+ str(self.StatusOfFile)
-        if self.StatusOfFile == True:
-	    self.newfilename=MTDI.newname(self.filenamesaved)
+	if self.extension=="ogg" or self.extension=="OGG":
+	    self.folderfileOGG=self.PathOfFile+'/' + quote(self.filenamesaved)
+	    import pdb; pdb.set_trace()
+	    self.prefiletranscoded=self.STORAGE+self.PathOfFile+'/'+self.filenamesaved
+	    if path.isfile(self.prefiletranscoded)==True:
+		import pdb; pdb.set_trace()
+		self.StatusOfFile=ServiceList.available(idaudio,self.prefiletranscoded)
+		if self.StatusOfFile == False:
+		    ServiceList.AddReadyElement(idaudio,self.prefiletranscoded)
+		    ServiceList.SaveInZODB()
+		    self.AbsoluteServerPath = self.SERVER + self.folderfileOGG
+		else:
+		    self.AbsoluteServerPath = self.SERVER + self.folderfileOGG
+	    else:
+		print _("File not found "+self.prefiletranscoded)
+		self.Error=True
+		self.ErrorSituation()
 	else:
-	    self.newfilename=_('The file is not ready yet, please contact site administration')
+	    newtrans_init_(self.STORAGE,
+			   self.PathOfFile,
+			   self.filenamesaved,
+			   idaudio,
+			   VIDEO_PARAMETRES_TRANSCODE,
+			   AUDIO_PARAMETRES_TRANSCODE,
+			   audio_content_types,
+			   video_content_types)
+	    self.folderfileOGG=MTDI.newname(self.PathOfFile+'/' + self.filenamesaved)
+	    self.AbsoluteServerPath = self.SERVER + MTDI.nginxpath(self.folderfileOGG)
+	    self.newfiletranscoded=MTDI.nginxpath(self.STORAGE+self.folderfileOGG)
+	    self.StatusOfFile = ServiceList.available(idaudio,self.newfiletranscoded)
+	    if self.StatusOfFile == True:
+		self.newfilename=MTDI.newname(self.filenamesaved)
+	    else:
+		self.newfilename=_('The file is not ready yet, please contact site administration')
 	return 
 
-
     def ErrorSituation(self):
-	import pdb; pdb.set_trace()
+	#import pdb; pdb.set_trace()
 	if self.Error==False:
 	    return False
 	else:
@@ -114,17 +134,29 @@ class audioView(BrowserView):
 
 
     def GETFileSize(self):
-        try:
-	    self.filesize = MFNI.ReturnFileSizeOfFileInHardDrive(self.newfiletranscoded)
-	    thefilesize = self.filesize
-	    return thefilesize
-	except OSError:
-	    self.Error=True
-	    self.ErrorSituation()
-	    return "0 kb"
+	if self.extension=='ogg':
+	    try:
+		self.filesize = MFNI.ReturnFileSizeOfFileInHardDrive(self.STORAGE+self.folderfileOGG)
+		thefilesize = self.filesize
+		return thefilesize
+	    except OSError:
+		self.Error=True
+		self.ErrorSituation()
+		return "0 kb"
+	else:
+	    try:
+		self.filesize = MFNI.ReturnFileSizeOfFileInHardDrive(self.newfiletranscoded)
+		thefilesize = self.filesize
+		return thefilesize
+	    except OSError:
+		self.Error=True
+		self.ErrorSituation()
+		return "0 kb"
 
     def GETAdressOfAudioFromApache(self):
-	
+	"""
+	This method return the server path to the <audio> label
+	"""
 	TheFilePath = self.AbsoluteServerPath
 	return TheFilePath
 
@@ -139,14 +171,7 @@ class audioView(BrowserView):
 	TheNewName = self.newfilename
 	return TheNewName
 
-    def GETfolderfile(self):
-	
-	TheFolderFile = self.AbsoluteServerPath 
-	return TheFolderFile 
-
-    
     def GETMyTitle(self):
-	
 	MyTitleWhitOutSpace  = self.MyTitleWhitOutSpace
 	return MyTitleWhitOutSpace
 
